e59848d9bcc8d087d4767906f326b0e088e67a2c
Leonardo Souza <leonardo.vilson@hotmail.com>
2022-06-11 21:36:23 -0300

    Revert "Merge pull request #8 from leonardoosouza1/development"
    This reverts commit d6bb49654f8725207239fb83016c7faa4ba56321.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
 /* eslint-disable camelcase */
 import React from 'react'
+import AppLoading from 'expo-app-loading'
+import {
+  useFonts,
+  Poppins_700Bold,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+  Poppins_500Medium
+} from '@expo-google-fonts/poppins'
 
-import { Routes } from './src/routes'
+import Routes from 'routes'
+
 import {
   ThemeProvider,
   AuthProvider
-} from './src/contexts'
+} from 'contexts'
 
 export default function App () {
+  const [loaded] = useFonts({
+    Poppins_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_500Medium
+  })
+
+  if (!loaded) {
+    return <AppLoading />
+  }
+
   return (
     <AuthProvider>
       <ThemeProvider>
