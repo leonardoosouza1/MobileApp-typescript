6b7b07e4421195e8be946035bb3602a699357e09
leonardoosouza1 <leonardo.vilson@hotmail.com>
2022-06-11 21:35:05 -0300

    feat: created routes and screens

--- App.tsx
+++ App.tsx
@@ -1,33 +1,13 @@
 /* eslint-disable camelcase */
 import React from 'react'
-import AppLoading from 'expo-app-loading'
-import {
-  useFonts,
-  Poppins_700Bold,
-  Poppins_400Regular,
-  Poppins_600SemiBold,
-  Poppins_500Medium
-} from '@expo-google-fonts/poppins'
 
-import Routes from 'routes'
-
+import { Routes } from './src/routes'
 import {
   ThemeProvider,
   AuthProvider
-} from 'contexts'
+} from './src/contexts'
 
 export default function App () {
-  const [loaded] = useFonts({
-    Poppins_700Bold,
-    Poppins_400Regular,
-    Poppins_600SemiBold,
-    Poppins_500Medium
-  })
-
-  if (!loaded) {
-    return <AppLoading />
-  }
-
   return (
     <AuthProvider>
       <ThemeProvider>
