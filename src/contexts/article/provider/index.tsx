37a3b44e69acecbab2e4aee0e503b0a9f593d4fb
Leonardo Souza <leonardo.vilson@hotmail.com>
2022-06-11 21:36:23 -0300

    Revert "Merge pull request #8 from leonardoosouza1/development"
    This reverts commit d6bb49654f8725207239fb83016c7faa4ba56321.

--- src/contexts/article/provider/index.tsx
+++ src/contexts/article/provider/index.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react'
-// import { createArticleService, getArticlesService, updateArticleService, deleteArticleService } from 'api/article'
+import { createArticleService, getArticlesService, updateArticleService, deleteArticleService } from 'api/article'
 
 import ArticleContext from '../context'
 
@@ -22,15 +21,13 @@ export type TArticleProvider = {
 const initialState = null
 
 const ArticleProvider = ({ children }: {children: React.ReactNode}) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // eslint-disable-next-line no-unused-vars
   const [ArticleData, setArticleData] = useState(initialState)
   const [loading, setLoading] = useState(false)
 
   const createArticle = async (title: string, description: string, body: string) => {
     setLoading(true)
 
-    // createArticleService(title, description, body).then(() => getArticles)
+    createArticleService(title, description, body).then(() => getArticles)
 
     setLoading(false)
   }
@@ -38,7 +35,7 @@ const ArticleProvider = ({ children }: {children: React.ReactNode}) => {
   const deleteArticle = async (id: string) => {
     setLoading(true)
 
-    // deleteArticleService(id).then(() => getArticles)
+    deleteArticleService(id).then(() => getArticles)
 
     setLoading(false)
   }
@@ -46,8 +43,8 @@ const ArticleProvider = ({ children }: {children: React.ReactNode}) => {
   const getArticles = async () => {
     setLoading(true)
 
-    // const { articles } = await getArticlesService()
-    // setArticleData(articles)
+    const { articles } = await getArticlesService()
+    setArticleData(articles)
 
     setLoading(false)
   }
@@ -55,20 +52,20 @@ const ArticleProvider = ({ children }: {children: React.ReactNode}) => {
   const updateArticle = async (id: string, title: string, description: string, body: string) => {
     setLoading(true)
 
-    // updateArticleService(id, title, description, body).then(() => {
-    //   const insertNewArticle = ArticleData.map((article: {id: string}) => {
-    //     if (article.id === id) {
-    //       return {
-    //         ...article,
-    //         title,
-    //         description,
-    //         body
-    //       }
-    //     }
-    //     return article
-    //   })
-    //   setArticleData(insertNewArticle)
-    // }).catch((error: Error) => console.error(error))
+    updateArticleService(id, title, description, body).then(() => {
+      const insertNewArticle = ArticleData.map((article: {id: string}) => {
+        if (article.id === id) {
+          return {
+            ...article,
+            title,
+            description,
+            body
+          }
+        }
+        return article
+      })
+      setArticleData(insertNewArticle)
+    }).catch((error: Error) => console.error(error))
 
     setLoading(false)
   }
